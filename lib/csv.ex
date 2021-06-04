fba72f8ab63c1683e35a14c97ff9af962d166368
Pakira002 <kiyomizuuta02@gmail.com>
2021-06-04 16:14:36 +0900

    firstcommit

---
+++ lib/csv.ex
@@ -0,0 +1,21 @@
+defmodule Csv do
+  def read_csv(filename) do
+    {status,contents}=File.read(filename)
+    case status do
+      :ok -> contents
+      :error -> "ファイル名が不正です。"
+    end
+  end
+
+  def make_list(contents) do
+    keys=
+    String.split(contents,"\r\n")
+    |> Enum.at(0)
+    |> String.split(",")
+    values=
+    String.split(contents,"\r\n")
+    |> List.delete_at(0)
+
+    {keys,values}
+  end
+end
